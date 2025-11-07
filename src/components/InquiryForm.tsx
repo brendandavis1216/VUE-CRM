dc16e060657ed66f72f29704d25070613d91fda0
[dyad] <brendan.davis.16@gmail.com>
2025-11-06 23:47:10 -0500

    [dyad] wrote 1 file(s)

--- src/components/InquiryForm.tsx
+++ src/components/InquiryForm.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * => z from "zod";
+import * as z from "zod"; // Corrected this line
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
